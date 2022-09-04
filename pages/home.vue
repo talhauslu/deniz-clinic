ffaeb229f16eaad2758418313a262b49909b63e5
burlesquel <batumanav@gmail.com>
2022-09-04 17:54:11 +0300

    landing page text color to white

--- pages/home.vue
+++ pages/home.vue
@@ -4,9 +4,9 @@
 
 <template>
     <div class="flex flex-row w-full overflow-hidden relative justify-between h-80vh">
-        <div class="flex flex-col justify-between 400:justify-around sm:justify-center sm:gap-8 w-full sm:w-1/2 h-full py-8 sm:py-0 px-8 z-10">
-            <h1 class="text-5xl sm:text-7xl text-gray-700 font-semibold">Sağlıkta dönüşümü keşfet</h1>
-            <h2 class="text-2xl sm:text-4xl">Her şey gülmekle başlar</h2>
+        <div class="flex flex-col justify-between 500:justify-around sm:justify-center sm:gap-8 w-full sm:w-1/2 h-full py-8 sm:py-0 px-8 z-10">
+            <h1 class="text-5xl sm:text-7xl text-white sm:text-gray-600 font-semibold">Sağlıkta dönüşümü keşfet</h1>
+            <h2 class="text-2xl sm:text-4xl text-white sm:text-black">İyi bir gülüş, iyi bir hayat </h2>
             <button
                 class="inline self-start p-4 rounded-lg bg-gray-600 font-semibold border text-white hover:bg-opacity-50 transition-all">
                 Randevu Al
@@ -15,7 +15,9 @@
         <video class="hidden sm:block absolute right-0 object-cover w-1/2 h-full object-center -z-10 border border-white" autoplay muted loop id="myVideo">
             <source src="~/assets/media/entrance_video.mp4" type="video/mp4">
         </video>
-        <nuxt-img class="block sm:hidden absolute object-cover w-full h-full object-center 500:object-left" 
+        <div class="w-full h-full block sm:hidden absolute bg-black">
+            <nuxt-img class=" object-cover w-full h-full object-center 500:object-left opacity-80" 
         src="/smiling_girl.jpg"/>
+        </div>
     </div>
 </template>
