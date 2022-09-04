634297870aabdaccb3f01efd7250074cad1bf470
burlesquel <batumanav@gmail.com>
2022-09-04 22:26:09 +0300

    milestone 1

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -2,18 +2,18 @@
     <footer class="text-gray-700">
         <div class="py-8 flex flex-col 500:flex-row gap-8 500:gap-0 justify-around w-full flex-wrap">
             <div class="flex flex-col gap-6 px-4">
-                <h1 class="text-2xl font-bold">Contact</h1>
+                <h1 class="text-2xl font-bold">İletişm</h1>
                 <div class="bg-gray-700 h-px w-full"></div>
                 <div class="flex flex-col gap-2">
-                    <h2 class="text-lg font-semibold">Phone</h2>
-                    <span>+561156151</span>
+                    <h2 class="text-lg font-semibold">Mobil</h2>
+                    <span>+123456789</span>
                 </div>
                 <div class="flex flex-col gap-2">
-                    <h2 class="text-lg font-semibold">Email</h2>
-                    <span>batumanav@gmail.com</span>
+                    <h2 class="text-lg font-semibold">E-mail</h2>
+                    <span>denizclinic@gmail.com</span>
                 </div>
                 <div class="flex flex-col gap-2">
-                    <h2 class="text-lg font-semibold">Social</h2>
+                    <h2 class="text-lg font-semibold">Sosyal Medya</h2>
                     <div class="flex flex-row items-center gap-2 text-2xl text-gray-600 ">
                         <i class="fa-brands fa-instagram cursor-pointer p-2 rounded-full hover:bg-gray-200"></i>
                         <i class="fa-brands fa-linkedin cursor-pointer p-2 rounded-full hover:bg-gray-200"></i>
@@ -23,25 +23,24 @@
             </div>
 
             <div class="flex flex-col gap-6 px-4">
-                <h1 class="text-2xl font-bold">Services</h1>
+                <h1 class="text-2xl font-bold">Servisler</h1>
                 <div class="bg-gray-700 h-px w-full"></div>
                 <div class="flex flex-col gap-4 font-semibold">
-                    <span>Dental Exam </span>
+                    <span>Dental Muayene </span>
                     <span>Invisalign® Aligners</span>
                     <span>Breezy Braces</span>
                     <span>Veneers</span>
                 </div>
             </div>
             <div class="flex flex-col gap-6 px-4">
-                <h1 class="text-2xl font-bold">Navigation</h1>
+                <h1 class="text-2xl font-bold">Navigasyon</h1>
                 <div class="bg-gray-700 h-px w-full"></div>
                 <div class="flex flex-col gap-4 font-semibold">
-                    <span>Career</span>
-                    <span>Whole Services</span>
-                    <span>Shop</span>
-                    <span>Insurance</span>
-                    <span>About</span>
-                    <span>Media</span>
+                    <span>Kariyer</span>
+                    <span>Tüm Servisler</span>
+                    <span>Hakkında</span>
+                    <span>Medya</span>
+                    <span>Önce & Sonra</span>
                 </div>
             </div>
 
