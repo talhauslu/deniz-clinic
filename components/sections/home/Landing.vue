f13c8e37ac2c2a644a38140b29af813c5bfba8ba
burlesquel <batumanav@gmail.com>
2022-09-09 05:17:00 +0300

    transitions done, landing is seperated

--- components/sections/home/Landing.vue
+++ components/sections/home/Landing.vue
@@ -4,22 +4,26 @@
 
 <template>
     <div class="flex flex-row w-full overflow-hidden relative justify-between h-80vh min-h-80vh">
-        <div data-aos="fade-right"
-            class="flex flex-col justify-between 500:justify-around sm:justify-center sm:gap-8 w-full sm:w-1/2 h-full p-2 sm:p-8 z-10">
-            <h1 class="text-white lg:text-gray-600 font-semibold">Sağlıkta dönüşümü keşfet</h1>
-            <h2 class="text-white lg:text-black">İyi bir gülüş, iyi bir hayat </h2>
-            <button
-                class="inline self-start p-4 rounded-lg bg-gray-600 font-semibold border text-white hover:bg-opacity-50 transition-all shadow-md">
-                Randevu Al
-            </button>
-        </div>
-        <video
-            class="hidden lg:block absolute right-0 object-cover w-1/2 h-full object-center -z-10 border border-white"
-            autoplay muted loop id="myVideo">
-            <source src="~/assets/media/entrance_video.mp4" type="video/mp4">
-        </video>
+        <transition appear name="swipe-from-left">
+            <div
+                class="flex flex-col justify-between 500:justify-around sm:justify-center sm:gap-8 w-full sm:w-1/2 h-full p-2 sm:p-8 z-10">
+                <h1 class="text-white lg:text-gray-600 font-semibold">Sağlıkta dönüşümü keşfet</h1>
+                <h2 class="text-white lg:text-black">İyi bir gülüş, iyi bir hayat </h2>
+                <button
+                    class="inline self-start p-4 rounded-lg bg-gray-600 font-semibold border text-white hover:bg-opacity-50 transition-all shadow-md">
+                    Randevu Al
+                </button>
+            </div>
+        </transition>
+        <transition appear name="swipe-from-right">
+            <video
+                class="hidden lg:block absolute right-0 object-cover w-1/2 h-full object-center -z-10 border border-white"
+                autoplay muted loop id="myVideo">
+                <source src="~/assets/media/entrance_video.mp4" type="video/mp4">
+            </video>
+        </transition>
         <div class="w-full h-full block lg:hidden absolute bg-black">
-            <nuxt-img  class="object-cover w-full h-full object-center opacity-80" src="/smiling_girl.jpg" />
+            <nuxt-img class="object-cover w-full h-full object-center opacity-80" src="/smiling_girl.jpg" />
         </div>
     </div>
 </template>
