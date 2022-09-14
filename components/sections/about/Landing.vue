fedfff53fdfa7df2c9d2ebebc2b2aca03f563fb5
burlesquel <batumanav@gmail.com>
2022-09-14 05:42:47 +0300

    about page git-col-reverse

--- components/sections/about/Landing.vue
+++ components/sections/about/Landing.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-    <section class="bg-white flex flex-col md:flex-row justify-around gap-4 md:gap-0 p-4">
+    <section class="bg-white flex flex-col-reverse md:flex-row justify-around gap-4 md:gap-0 p-4">
         <div class="animation-element fade-left w-full md:w-1/2 flex flex-col justify-center gap-8 ">
             <h1 class="font-light text-4xl ">
                 Uzm. Dr. Deniz Yılmaz
